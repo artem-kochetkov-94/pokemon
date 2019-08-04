391ee59bfa4463276ac807fc7b229fd8e56b6335
Артём Кочетков <artemkocetkov@MacBook-Pro-Artem.local>
2019-08-04 23:02:59 +0300

    part

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import Home from "./pages/home/index"
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "~/pages/home/index"
+// import Profile from "~/pages/profile/index"
+import { Router, Route, Switch } from "react-router"
+
+function Lala() {
+    return <h1>123</h1>
+}
 
 function App() {
-    return
-    <Router>
-        <Route path="/" exact component={Home} />
-        <Route path="/Profile/" component={Profile} />
-    </Router>
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/profile" component={Lala} />
+            </Switch>
+        </Router>
+    )
 }
 
 ReactDOM.render(<App />, document.getElementById('app'))
---
+++ src/pages/Profile/index.js
@@ -0,0 +1,27 @@
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import { SvgIcon } from '@material-ui/core';
+
+function HomeIcon(props) {
+    return (
+        <SvgIcon {...props}>
+            <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+        </SvgIcon>
+    );
+}
+
+export default function Profile() {
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h6" noWrap>
+                    Material-UI
+          </Typography>
+            </Toolbar>
+            <IconButton aria-label="" color="inherit">
+                <HomeIcon></HomeIcon>
+            </IconButton>
+        </AppBar>
+    )
+}
